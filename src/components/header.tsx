459e041f4345f6c52af27af6fdc89cd97ca799ea
iwatomo8 <tomo090818@gmail.com>
2025-01-19 20:01:52 +0900

    feat: アプリ名クリックでトップページに遷移するようにリンク追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import { LoginButton } from "@/components/login-button";
 import { Notifications } from "@/components/notifications";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export function Header() {
   const { data: session } = useSession();
@@ -11,9 +12,9 @@ export function Header() {
   return (
     <header className="border-b bg-card text-card-foreground shadow-sm">
       <div className="flex h-16 items-center px-6 md:px-6 pl-16 md:pl-6">
-        <h1 className="text-lg font-semibold md:ml-64">
+        <Link href="/" className="text-lg font-semibold md:ml-64">
           きのどくなー 管理画面
-        </h1>
+        </Link>
         <div className="ml-auto flex items-center space-x-4">
           {session && <Notifications />}
           <LoginButton />
